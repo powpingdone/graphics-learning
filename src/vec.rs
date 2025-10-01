154f5d2acf4f8ac47d992ba9affb953523ae2847
Aidan <aidanzcase@gmail.com>
2025-10-01 17:37:51 -0500

    Lots of ops

--- src/vec.rs
+++ src/vec.rs
@@ -14,7 +14,7 @@ struct Vect<I: ArrayLength + Unsigned> {
 impl<I: ArrayLength + Unsigned> Vect<I> {
     pub fn new() -> Self {
         const {
-            assert!(<I as Unsigned>::I32 < 5 && <I as Unsigned>::I32 > 0);
+            assert!(<I as Unsigned>::USIZE < 5 && <I as Unsigned>::USIZE > 0);
         }
         Self {
             vec: GenericArray::default(),
@@ -126,14 +126,77 @@ struct Mat<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> {
 
 impl<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> Mat<X, Y> {
     pub fn new() -> Self {
+        const {
+            assert!(<X as Unsigned>::USIZE < 5 && <X as Unsigned>::USIZE > 0);
+            assert!(<Y as Unsigned>::USIZE < 5 && <Y as Unsigned>::USIZE > 0);
+        }
         Self {
             mat: GenericArray::default(),
         }
     }
+
+    pub fn t(self) -> Mat<Y, X> {
+        let mut ret = Mat::new();
+        for x in 0..X::USIZE {
+            for y in 0..Y::USIZE {
+                ret[y][x] = self[x][y];
+            }
+        }
+        ret
+    }
 }
 
-impl<X: ArrayLength + Unsigned, Y:  ArrayLength + Unsigned> Default for Mat<X, Y> {
+impl<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> Index<usize> for Mat<X, Y> {
+    type Output = Vect<Y>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.mat[index]
+    }
+}
+
+impl<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> IndexMut<usize> for Mat<X, Y> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.mat[index]
+    }
+}
+
+impl<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> Default for Mat<X, Y> {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+// standard ops
+impl<X: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> Add<Mat<X, Y>> for Mat<X,Y> {
+    type Output = Mat<X,Y>;
+
+    fn add(mut self, rhs: Mat<X, Y>) -> Self::Output {
+        for x in 0..X::USIZE {
+            for y in 0..Y::USIZE {
+                self[x][y] += rhs[x][y];
+            }
+        }
+        self
+    }
+}
+
+// matmul
+impl<X: ArrayLength + Unsigned, M: ArrayLength + Unsigned, Y: ArrayLength + Unsigned> Mul<Mat<M, Y>>
+    for Mat<X, M>
+{
+    type Output = Mat<X, Y>;
+
+    fn mul(self, rhs: Mat<M, Y>) -> Self::Output {
+        let mut ret = Mat::new();
+        // [[f32; Y]; X] * [[f32; Y2]; Y] = [[f32; Y2]; X]
+        // self[X][Y] * rhs[Y][Y2] = new[X][Y2]
+        for x in 0..X::USIZE {
+            for y in 0..Y::USIZE {
+                for i in 0..M::USIZE {
+                    ret[x][y] += self[x][i] * rhs[i][y];
+                } 
+            }
+        }
+        ret 
     }
 }
